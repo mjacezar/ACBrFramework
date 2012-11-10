de97b8695aa5e1466db5ecfe087865f2b9d20ee4
batiati <batiati@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2012-11-10 00:33:53 +0000

    [*] Modificação em InfoECFEventArgs para funcionar conforme o evento em Delphi - Rafael Batiati

--- ACBrFramework.Net/ACBrFramework.Net/TEFD/InfoECFEventArgs.cs
+++ ACBrFramework.Net/ACBrFramework.Net/TEFD/InfoECFEventArgs.cs
@@ -19,10 +19,88 @@ namespace ACBrFramework.TEFD
 
 		public string Retorno { get; set; }
 
-		public RetornoECF? RetornoECF { get; set; }
+		public RetornoECF? RetornoECF 
+		{
+			get
+			{
+				return GetAsRetornoECF();
+			}
+			set
+			{
+				SetAsRetornoECF(value);
+			}
+		}
 
-		public decimal? Value { get; set; }
+		public decimal? Value 
+		{
+			get
+			{
+				return GetAsDecimal();
+			}
+			set
+			{
+				SetAsDecimal(value);
+			}
+		}
+
 
 		#endregion Properties
+
+		#region Methods
+
+		private RetornoECF? GetAsRetornoECF()
+		{
+			if (string.IsNullOrEmpty(Retorno)) return null;
+
+			if (Retorno.Length == 1)
+			{
+				int value = (int)Retorno[0];
+				if (Enum.IsDefined(typeof(RetornoECF), value))
+				{
+					return (RetornoECF)value;
+				}
+			}
+
+			return null;
+		}
+		
+		private void SetAsRetornoECF(RetornoECF? value)
+		{
+			if (value == null)
+			{
+				Retorno = null;
+			}
+			else
+			{
+				Retorno = ((char)value.Value).ToString();
+			}
+		}
+
+		private decimal? GetAsDecimal()
+		{
+			decimal value;
+			if (decimal.TryParse(Retorno, out value))
+			{
+				return value;
+			}
+			else
+			{
+				return null;
+			}
+		}
+
+		private void SetAsDecimal(decimal? value)
+		{
+			if (value == null)
+			{
+				Retorno = null;
+			}
+			else
+			{
+				Retorno = string.Format("{0:n2}", value);
+			}
+		}
+		
+		#endregion Methods
 	}
 }
