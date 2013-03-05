cf4dbb4d78127b0d405aa3ec1845157388a596df
batiati <batiati@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2013-03-05 02:30:51 +0000

    Inclusão do componente ACBrDIS - Rafael Batiati.

---
+++ ACBrFramework.Net/ACBrFramework.Net/Serial/DIS/Classes/EfeitoRolar.cs
@@ -0,0 +1,11 @@
+﻿namespace ACBrFramework.DIS
+{
+	public enum EfeitoRolar : int
+	{
+		ParaEsquerda_Inicio = 0,
+		ParaEsquerda_Sempre = 1,
+		ParaDireita_Inicio = 2,
+		ParaDireita_Sempre = 3,
+		Vai_e_Volta = 4
+	}
+}
---
+++ ACBrFramework.Net/ACBrFramework.Net/Serial/DIS/Classes/ModeloDIS.cs
@@ -0,0 +1,11 @@
+﻿namespace ACBrFramework.DIS
+{
+	public enum ModeloDIS : int
+	{
+		Nenhum = 0,
+		GertecSerial = 1,
+		GertecTeclado = 2,
+		KeytecTeclado = 3,
+		SmakTeclado = 4
+	}
+}
