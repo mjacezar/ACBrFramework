18a25207ceb52a576513e8c6f3605feb0f9f7901
batiati <batiati@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2013-04-18 13:20:30 +0000

    Adicionado suporte a COMINTEROP no componente ACBrDIS - Rafael Batiati

--- ACBrFramework/ACBrFramework.Net/ACBrFramework.Net/Serial/DIS/Eventos/AtualizaEventArgs.cs
+++ ACBrFramework/ACBrFramework.Net/ACBrFramework.Net/Serial/DIS/Eventos/AtualizaEventArgs.cs
@@ -1,7 +1,28 @@
 ﻿using System;
 
+#region COM_INTEROP
+
+#if COM_INTEROP
+
+using System.Runtime.InteropServices;
+
+#endif
+
+#endregion COM_INTEROP
+
 namespace ACBrFramework.DIS
 {
+	#region COM_INTEROP
+
+#if COM_INTEROP
+
+	[ComVisible(true)]
+	[Guid("11D39543-D08B-4D94-A80A-386A4825564D")]
+	[ClassInterface(ClassInterfaceType.AutoDual)]
+#endif
+
+	#endregion COM_INTEROP
+
 	public class AtualizaEventArgs : EventArgs
 	{
 		public int Linha { get; internal set; }
