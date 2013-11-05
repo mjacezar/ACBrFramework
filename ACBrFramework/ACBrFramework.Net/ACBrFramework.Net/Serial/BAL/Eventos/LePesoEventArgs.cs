6f50f83e4cabcf5c3e0457f9164008e701ff04f9
rfteno <rfteno@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2013-11-05 22:36:21 +0000

    -- ACBrFramework 0.9.4.5 -- [+] Adicionado suporte ao COMInterop - ACBrLCB - Rafael Dias. [+] Adicionado suporte ao COMInterop - ACBrBAL - Rafael Dias.

--- ACBrFramework/ACBrFramework.Net/ACBrFramework.Net/Serial/BAL/Eventos/LePesoEventArgs.cs
+++ ACBrFramework/ACBrFramework.Net/ACBrFramework.Net/Serial/BAL/Eventos/LePesoEventArgs.cs
@@ -1,9 +1,48 @@
 ﻿using System;
+#region COM_INTEROP
+
+#if COM_INTEROP
+
+using System.Runtime.InteropServices;
+
+#endif
+
+#endregion COM_INTEROP
 
 namespace ACBrFramework.BAL
 {
+    #region COM Interop Attributes
+
+#if COM_INTEROP
+
+    [ComVisible(true)]
+    [Guid("B1357CB5-02DA-4274-B2AC-13EA5D623153")]
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+
+#endif
+
+    #endregion COM Interop Attributes
 	public class LePesoEventArgs : EventArgs
 	{
-		public decimal Peso { get; set; }
+		public decimal Peso
+        {
+            #region COM_INTEROP
+
+#if COM_INTEROP
+			[return: MarshalAs(UnmanagedType.Currency)]
+#endif
+
+                #endregion COM_INTEROP
+            get; 
+            
+            #region COM_INTEROP
+
+#if COM_INTEROP
+			[return: MarshalAs(UnmanagedType.Currency)]
+#endif
+
+                #endregion COM_INTEROP
+            set;
+        }
 	}
 }
