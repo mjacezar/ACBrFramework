b09e28fc7fc7ca99cf8cea5e2c8802ba44d0cde2
rfteno <rfteno@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2015-02-20 01:15:34 +0000

    Adicionado arquivo que faltou no ultimo commit.

---
+++ ACBrFramework/ACBrFramework.Net/ACBrFramework.Net/ACBrFramework/ACBrExpandableObjectConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+
+namespace ACBrFramework
+{
+    public class ACBrExpandableObjectConverter : ExpandableObjectConverter
+    {
+        /// <summary>
+        /// Converts to.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <param name="culture">The culture.</param>
+        /// <param name="value">The value.</param>
+        /// <param name="destType">Type of the dest.</param>
+        /// <returns>System.Object.</returns>
+        public override object ConvertTo(ITypeDescriptorContext context, CultureInfo culture, object value, Type destType)
+        {
+            if ((value != null) && (destType == typeof(string)))
+            {
+                return (String.Format("({0})", value.GetType().Name));
+            }
+            return base.ConvertTo(context, culture, value, destType);
+        }
+    }
+}
